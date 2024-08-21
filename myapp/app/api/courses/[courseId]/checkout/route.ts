8e917c589071f4c73f7d06fc798eecc7bbbbd703
stino-x <107426247+stino-x@users.noreply.github.com>
2024-08-22 00:27:48 +0200

    feat: Update checkout route URLs for success and cancellation

--- myapp/app/api/courses/[courseId]/checkout/route.ts
+++ myapp/app/api/courses/[courseId]/checkout/route.ts
@@ -27,12 +27,12 @@ export async function POST(req: Request, { params }: { params: { courseId: strin
             return new NextResponse('Not Found', { status: 404 });
         }
 
-        const baseUrl = process.env.NEXT_PUBLIC_APP_URL?.endsWith('/')
-  ? process.env.NEXT_PUBLIC_APP_URL
-  : `${process.env.NEXT_PUBLIC_APP_URL}/`;
+        const successUrl = encodeURI(`${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}/success=1`);
+        const cancelUrl = encodeURI(`${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}/canceled=1`);
+        
 
-const successUrl = `${baseUrl}courses/${course.id}/success=1`;
-const cancelUrl = `${baseUrl}courses/${course.id}/canceled=1`;
+       // const successUrl = `https://learn-city.vercel.app/courses/${course.id}/success=1`;
+       // const cancelUrl = `https://learn-city.vercel.app/courses/${course.id}/canceled=1`;
 
 
         // Debugging lines to check the URLs
