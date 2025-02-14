e8fe6fce2b3851532dca9b4ae870b087e61a0257
stino-x <107426247+stino-x@users.noreply.github.com>
2025-02-14 17:02:11 +0100

    Refactor course retrieval to use ORM for improved readability and maintainability

--- myapp/actions/get-courses.tsx
+++ myapp/actions/get-courses.tsx
@@ -16,13 +16,52 @@ type GetCourses = {
 
 export const getCourses = async ({userId, title, categoryId}: GetCourses): Promise<CourseWithProgressWithCategory[]> => {
     try {
-        const courses = await db.$queryRaw`
-  SELECT * FROM "Course"
-  WHERE "isPublished" = true
-  AND to_tsvector('english', title) @@ plainto_tsquery(${title})
-  ${categoryId ? `AND "categoryId" = ${categoryId}` : ''}
-  ORDER BY "createdAt" DESC
-`;
+        // const courses = await db.$queryRaw<CourseWithProgressWithCategory[]>`
+        //     SELECT 
+        //         c.*,
+        //         cat.id AS "categoryId",
+        //         cat.name AS "categoryName",
+        //         ch.id AS "chapterId",
+        //         p.id AS "purchaseId"
+        //     FROM "Course" c
+        //     LEFT JOIN "Category" cat ON c."categoryId" = cat.id
+        //     LEFT JOIN "Chapter" ch ON c.id = ch."courseId" AND ch."isPublished" = true
+        //     LEFT JOIN "Purchase" p ON c.id = p."courseId" AND p."userId" = ${userId}
+        //     WHERE c."isPublished" = true
+        //     AND to_tsvector('english', c.title) @@ plainto_tsquery(${title})
+        //     ${categoryId ? `AND c."categoryId" = ${categoryId}` : ''}
+        //     ORDER BY c."createdAt" DESC
+        // `;
+
+
+        const courses = await db.course.findMany({
+            where: {
+                isPublished: true,
+                title: {
+                    contains: title,
+                },
+                categoryId: categoryId,
+            },
+            include: {
+                category: true,
+                chapters: {
+                    where: {
+                        isPublished: true,
+                    },
+                    select: {
+                        id: true,
+                    },
+                },
+                purchases: {
+                    where: {
+                        userId,
+                    },
+                },
+            },
+            orderBy: {
+                createdAt: 'desc',
+            },
+        });
 
 
         const coursesWithProgress: CourseWithProgressWithCategory[] = await Promise.all(courses.map(async (course) => {
